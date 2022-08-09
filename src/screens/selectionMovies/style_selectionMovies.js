4af1104b8bfd18078cb4d180fd4631fb17721e1b
Arthur Loyola <arthurloyola@hotmail.com>
2022-08-08 22:55:44 -0300

    Teste1

---
+++ src/screens/selectionMovies/style_selectionMovies.js
@@ -0,0 +1,58 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#000', 
+        paddingHorizontal:15,
+    },
+    header: {
+        marginTop: 60,
+        alignItems:'flex-start',
+        marginHorizontal:10
+    },
+    text: {
+        fontSize: 20,
+        color: 'red'
+    },
+    containerRow: {
+        flexDirection: 'row',
+    },
+    title: {
+        fontSize: 22,
+        fontWeight: '700',
+        lineHeight: 25,
+        color: '#FFF'
+    },
+    label: {
+        fontSize: 22,
+        fontWeight: '700',
+        lineHeight: 25,
+        color: '#E9A6A6',
+        marginLeft: 3,
+    },
+    description: {
+        fontSize: 14,
+        fontWeight: '400',
+        lineHeight: 25,
+        color: '#FFF',
+        marginTop: 8
+    },
+    popularMovies: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#FFF',
+        marginTop: 30,
+        marginBottom: 40
+    },
+    containerCardMovies:{
+        justifyContent:'space-between', 
+        alignItems:'center', 
+        flex:1
+    },
+    section:{
+    width:'100%', 
+   }
+})
+
+export default styles
