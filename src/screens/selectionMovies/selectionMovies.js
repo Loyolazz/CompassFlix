24239c4b450af08a4c23652a723e7fe7d99d8e26
Arthur Loyola <arthurloyola@hotmail.com>
2022-08-10 14:32:54 -0300

    correcoe3

--- src/screens/selectionMovies/selectionMovies.js
+++ src/screens/selectionMovies/selectionMovies.js
@@ -3,11 +3,27 @@ import { View, FlatList, TouchableOpacity} from 'react-native';
 import { CardMovies } from '../../Components/selectMoviesComp/cards/cardMovies';
 import Header from '../../Components/selectMoviesComp/header/header'
 import api from '../../services/api';
+import styles from './style_selectionMovies';
 
 const apikey =  'api_key=80eb37af6714ab187d2c58f9acc83af3';
 const language = 'language=pt-BR';
 const keyApiGet =  `/popular?${apikey}&${language}&page=${1}`
 
+export function SelectionMovies({navigation}) {
+  const [selectedId, setSelectedId] = useState(null);
+  const [listApi, setListApi] = useState([]);
+  const nameUser = 'John'
+
+  const init = async () => {
+    const response = await api.get(keyApiGet)
+    console.log(response);
+    setListApi(response.data.results);
+    console.log('Request success')
+  };
+
+  useEffect(() => {
+    init();
+  }, [])
 
   return (
 
