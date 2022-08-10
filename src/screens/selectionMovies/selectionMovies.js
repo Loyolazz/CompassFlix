e644965373162beb5eb3e9ce134d5fcc0d8ad5c3
Arthur Loyola <arthurloyola@hotmail.com>
2022-08-10 14:31:12 -0300

    correcoes2

--- src/screens/selectionMovies/selectionMovies.js
+++ src/screens/selectionMovies/selectionMovies.js
@@ -3,13 +3,12 @@ import { View, FlatList, TouchableOpacity} from 'react-native';
 import { CardMovies } from '../../Components/selectMoviesComp/cards/cardMovies';
 import Header from '../../Components/selectMoviesComp/header/header'
 import api from '../../services/api';
-import styles from './style_selectionMovies';
 
 const apikey =  'api_key=80eb37af6714ab187d2c58f9acc83af3';
 const language = 'language=pt-BR';
 const keyApiGet =  `/popular?${apikey}&${language}&page=${1}`
 
-export function SelectionMovies({navigation}) {
+
   return (
 
     <View style={styles.container}>
--- src/services/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const Api = axios.create({
-    baseURL:`https://api.themoviedb.org/3/movie`
-})
-export default Api; 
