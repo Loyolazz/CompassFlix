d3d6e59e42d8eab1fcaa3c41b930eda4a58d2ccc
Marlison Mourao <bentesmourao@gmail,com>
2022-08-11 02:21:22 -0400

    tela de spash

---
+++ src/screens/Splash/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, Image} from 'react-native';
+
+import LottieView from 'lottie-react-native';
+import logo from '../../assets/login_imagens/logo.png';
+
+export function Splash({navigation}) {
+  const FinashSplach = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{name: 'Login'}],
+    });
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000',
+      }}>
+      <Image source={logo} style={{}} />
+      <LottieView
+        source={require('../../assets/splach.json')}
+        autoPlay
+        loop={false}
+        speed={2.5}
+        autoSize={true}
+        style={{height: 250}}
+        onAnimationFinish={() => FinashSplach()}
+      />
+    </View>
+  );
+}
