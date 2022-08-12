6bbc1293008afe7c3d26e8a86080074f1379162a
Marlison Mourao <bentesmourao@gmail,com>
2022-08-11 21:40:14 -0400

    correções

--- src/screens/selectionMovies/selectionMovies.js
+++ src/screens/selectionMovies/selectionMovies.js
@@ -1,40 +1,45 @@
-import React, {useState, useEffect} from 'react';
-import {View, FlatList} from 'react-native';
-import CardMovies from '../../components/selectMoviesComp/cards/cardMovies'
-import Load from '../../components/Load'
-import Loading from '../../components/Loading'
-import HeaderFilm from '../../components/selectMoviesComp/header/header'
+import React, {useState, useEffect, useContext} from 'react';
+import {View, FlatList, Text} from 'react-native';
+import CardMovies from '../../Components/selectMoviesComp/cards/cardMovies';
+import Load from '../../Components/Load';
+import Loading from '../../Components/Loading';
+import HeaderFilm from '../../Components/selectMoviesComp/header/header';
+import {Context} from '../../context';
 import styles from './style_selectionMovies';
-import { getMovies } from '../../services/api';
-
+import {getMovies, getAccount} from '../../services/api';
 
 export function SelectionMovies({navigation}) {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [selectedId, setSelectedId] = useState(null);
-
-  const nameUser = 'John';
+  const [selected, setSelected] = useState();
+  const {id} = useContext(Context);
 
   const getResponseMovies = async () => {
-    if (loading) {
-      return;
-    }
-    setLoading(true);
+
 
     const response = await getMovies(page);
     setMovies([...movies, ...response.data.results]);
     setPage(page + 1);
-    setLoading(false);
   };
+
+  useEffect(() => {
+    const getResponseAccount = async () => {
+      const response = await getAccount(id);
+      setSelected(response.data.name);
+    };
+    getResponseAccount();
+  }, [id]);
 
   useEffect(() => {
     getResponseMovies();
   }, []);
-  return movies ? (
+
+  return movies && selected ? (
     <View style={styles.container}>
       <View style={styles.section}>
-        <HeaderFilm nameUser={nameUser} />
+        <HeaderFilm nameUser={selected} />
       </View>
 
       <FlatList
@@ -48,21 +53,19 @@ export function SelectionMovies({navigation}) {
         onEndReached={getResponseMovies}
         onEndReachedThreshold={0.1}
         ListFooterComponent={<Loading load={loading} />}
-
-
         renderItem={({item}) => {
-          const id = `${item.id}`
-          return(
-          <CardMovies
-            text={`${item.vote_average}`}
-            poster_path={item.poster_path}
-            onPress={() => {
-              setSelectedId(id)
-              console.log('trocou')
-              navigation.navigate('MoviesDetail', { item })
-            }}
-          />
-  )}}
+          const id = `${item.id}`;
+          return (
+            <CardMovies
+              text={`${item.vote_average}`}
+              poster_path={item.poster_path}
+              onPress={() => {
+                setSelectedId(id);
+                navigation.navigate('MoviesDetail', {item});
+              }}
+            />
+          );
+        }}
       />
     </View>
   ) : (
