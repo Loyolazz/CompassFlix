9e63b29bbadb86d4cc87ac04f933f0e5188cf851
Marlison Mourao <bentesmourao@gmail,com>
2022-08-16 22:12:34 -0400

    adicionando as novas telas e configurando tabBar

---
+++ src/screens/selectionSeries/selectionSeleries.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function SelectionSeries() {
+    return(
+     <View  style={{flex: 1, backgroundColor: '#000', alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontWeight: 'bold', fontSize: 20}} >Series</Text>
+     </View>
+    )
+}
