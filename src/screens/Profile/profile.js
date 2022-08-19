ada35dca6243ded91106f2536d61d33eba995778
Henrique Silva <henriques748@gmail.com>
2022-08-19 08:07:55 -0400

    adicionando cor no butao

--- src/screens/Profile/profile.js
+++ src/screens/Profile/profile.js
@@ -1,62 +1,71 @@
-import React, { useState } from 'react'
-import { View,TouchableOpacity} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import styles from './styleProfile'
-import photUser from '../../assets/imgUser/perfil_photo.png'
-import HeaderProfile from '../../Components/ProfileComp/header/headerProfile'
-import MoviesList from '../../Components/ProfileComp/MoviesList/moviesList'
-import SeriesList from '../../Components/ProfileComp/SerieList/serieList'
+import React, {useState} from 'react';
+import {View, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import styles from './styleProfile';
+import photUser from '../../assets/imgUser/perfil_photo.png';
+import HeaderProfile from '../../Components/ProfileComp/header/headerProfile';
+import MoviesList from '../../Components/ProfileComp/MoviesList/moviesList';
+import SeriesList from '../../Components/ProfileComp/SerieList/serieList';
 export default function Profile({navigation}) {
-    const[colorBtn, setColorBtn] = useState('grey')
-    const MoviesListDeafult = <MoviesList
-    onPressFavorite={() => navigation.navigate('MoviesFavorites')}
-    onPressEvaluation={() => navigation.navigate('EvaluationMovies')}
+  const [colorBtn, setColorBtn] = useState({
+    moviesButton:"#00AEFF",
+    seriesButton:"#5C5C5C"
+  });
+  const MoviesListDeafult = (
+    <MoviesList
+      onPressFavorite={() => navigation.navigate('MoviesFavorites')}
+      onPressEvaluation={() => navigation.navigate('EvaluationMovies')}
     />
-    const SeriesListDeafult = <SeriesList
-    onPressFavorite={() => navigation.navigate('SeriesFavorites')}
-    onPressEvaluation={() => navigation.navigate('EvaluationSeries')}
+  );
+  const SeriesListDeafult = (
+    <SeriesList
+      onPressFavorite={() => navigation.navigate('SeriesFavorites')}
+      onPressEvaluation={() => navigation.navigate('EvaluationSeries')}
     />
-    const [listView, setListView] = useState(MoviesListDeafult)
-   
-    const nameUser = 'John'
-    const photo = photUser
+  );
+  const [listView, setListView] = useState(MoviesListDeafult);
 
-    function MoviesListPress() {
-        setListView(MoviesListDeafult)
-    }
-    function SeriesListPress() {
-        setListView(SeriesListDeafult)
-    }
-    return (
-        <View style={styles.container}>
+  const nameUser = 'John';
+  const photo = photUser;
 
-            <HeaderProfile
-                nameUser={nameUser}
-                textEvaluation={'Avaliação'}
-                numberEvaluation={30}
-                photoUser={photo}
-            />
+  function MoviesListPress() {
+    setListView(MoviesListDeafult);
+    setColorBtn({moviesButton:"#00AEFF", seriesButton:"#5C5C5C"})
+  }
+  function SeriesListPress() {
+    setListView(SeriesListDeafult);
+    setColorBtn({moviesButton:"#5C5C5C", seriesButton:"#00AEFF"})
+  }
+  return (
+    <View style={styles.container}>
+      <HeaderProfile
+        nameUser={nameUser}
+        textEvaluation={'Avaliação'}
+        numberEvaluation={30}
+        photoUser={photo}
+      />
 
+      <View style={styles.containerFavoritesAndEvaluation}>
+        <View style={styles.containerIcons}>
+          <View style={styles.containerIconPopcorn}>
+            <TouchableOpacity onPress={MoviesListPress}>
+              <Icon
+                name="popcorn"
+                size={34}
+                color={colorBtn.moviesButton}
+                selectionColor={'#000'}
+              />
+            </TouchableOpacity>
+          </View>
 
-            <View style={styles.containerFavoritesAndEvaluation}>
-                <View style={styles.containerIcons}>
-
-                    <View style={styles.containerIconPopcorn}>
-                        <TouchableOpacity onPress={MoviesListPress}>
-                            <Icon name='popcorn' size={34} color={'grey'} selectionColor={'#000'} />
-                        </TouchableOpacity>
-                    </View>
-
-                    <View style={styles.containerIconPlay}>
-                        <TouchableOpacity onPress={SeriesListPress}>
-                            <Icon name='movie-play' size={34} color={'grey'} />
-                        </TouchableOpacity>
-                    </View>
-                </View>
-                <View>{listView}</View>
-              
-
-            </View>
+          <View style={styles.containerIconPlay}>
+            <TouchableOpacity onPress={SeriesListPress}>
+              <Icon name="movie-play" size={34} color={colorBtn.seriesButton} />
+            </TouchableOpacity>
+          </View>
         </View>
-    )
+        <View>{listView}</View>
+      </View>
+    </View>
+  );
 }
