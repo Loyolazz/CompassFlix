54d7535c393b782410b3dbbb287a3107d9f8b634
Angeluz20 <66801548+Angeluz20@users.noreply.github.com>
2022-09-07 17:32:56 -0300

    feat: SeriesDetails novo component

---
+++ src/Components/SeriesDetailsComp/Header/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Button, Image, Text, View} from 'react-native';
+
+import HeartIcon from 'react-native-vector-icons/FontAwesome';
+import styles from './styles';
+
+export function HeaderDetails({
+  Cartaz,
+  Director,
+  Nota,
+  Votes,
+  Year,
+  Duration,
+  TitleFilm,
+}) {
+  return (
+    <View style={styles.containerCartazAndTitle}>
+      <View style={styles.styleViewCartaz}>
+        <Image source={{uri: Cartaz}} style={styles.styleImgCartaz} />
+      </View>
+      <View style={styles.containerInfoFilm}>
+        <View style={styles.containerTitleFilm}>
+          <View style={styles.titleFilm}>
+            <Text style={styles.titleFilmView}>{TitleFilm}</Text>
+          </View>
+
+          <View style={styles.containerYearAndDuration}>
+            <Text style={styles.textYear}>{Year}</Text>
+            <Text style={styles.textDurantion}>{Duration}</Text>
+          </View>
+        </View>
+        <View style={{paddingHorizontal: 10}}>
+          <Text style={styles.directorName}>
+            Direção por <Text style={{fontWeight: 'bold'}}>{Director}</Text>
+          </Text>
+        </View>
+        <View style={styles.containerIconAndNote}>
+          <Text style={styles.styleNota}>{Nota}</Text>
+          <View style={styles.heart}>
+            <HeartIcon name="heart" size={30} color={'red'} />
+            <Text style={styles.votes}>{Votes}k</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
---
+++ src/Components/SeriesDetailsComp/Header/styles.js
@@ -0,0 +1,88 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+
+    containerCartazAndTitle: {
+        flexDirection: 'row',
+        paddingHorizontal: 20,
+        height:'13%'
+    },
+    styleViewCartaz: {
+        width: '30%',
+        height: '35%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    styleImgCartaz: {
+        borderRadius: 6,
+        height: 182,
+        width: 116,
+    },
+    styleNota: {
+        fontSize: 30,
+        color: '#E9A6A6',
+        marginRight: 50
+    },
+    containerInfoFilm: {
+        width: '50%',
+        height: '50%',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        flexDirection: 'column',
+        marginLeft:10
+    },
+    containerTitleFilm:{
+        flexDirection:'row', 
+        alignItems:'center',
+        //paddingHorizontal:20
+    },
+    titleFilm:{
+        height: 40 , 
+        alignItems:'flex-start', 
+        flexDirection:'column',  
+        justifyContent:'center', 
+        paddingHorizontal:10
+    },
+    titleFilmView: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: 'white'
+    },
+
+    directorName: {
+        color: 'white', 
+        fontSize: 8
+    },
+    containerIconAndNote:{
+        flexDirection: 'row', 
+        marginTop: 13,
+        paddingHorizontal:10
+    },
+    containerYearAndDuration:{
+        flexDirection:'row', 
+        justifyContent:'space-between', 
+        marginRight:20, 
+        alignItems:'center',
+        height:40 ,   
+        paddingHorizontal:10,
+    },
+    textYear:{
+        color:'#fff', 
+        fontSize:12,
+        marginRight:13
+    },
+    textDurantion:{
+        color:'#fff', 
+        fontSize:10
+    },
+    votes:{
+        fontSize: 13, 
+        color: 'white'
+    },
+    heart:{
+        justifyContent: 'flex-end', 
+        alignItems: 'center', 
+        height: 40 
+    }
+
+})
